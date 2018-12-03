65d8b807a9bc083481e75e67c2738f0ad0241853
evanwill <evanpeterw@gmail.com>
2018-12-02 23:14:54 -0800

    update value cleaning

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -10,7 +10,7 @@ var store = [
         "creator": {{ item.creator | jsonify }},
         "subjects": {{ item.subject | split: ";" | jsonify }},
         "date": {{ item.date | jsonify }},
-        "description": {{ item.description | jsonify }},
+        "description": {{ item.description | normalize_whitespace | jsonify }},
         "place": {{ item.place | jsonify }}
     }{%- unless forloop.last -%},{%- endunless -%}
     {%- endfor -%}
