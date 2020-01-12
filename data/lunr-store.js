443023d6ade7dbad2ff6625a19ff0403df829754
evanwill <evanpeterw@gmail.com>
2020-01-11 19:57:10 -0800

    update search config

--- data/lunr-store.js
+++ data/lunr-store.js
@@ -2,7 +2,7 @@
 # create lunr store for search page
 ---
 {%- assign items = site.data[site.metadata] -%}
-{%- assign fields = site.data.search-config -%}
+{%- assign fields = site.data.config-search -%}
 var store = [ 
 {%- for item in items -%} 
 { "id": {{ item.objectid | jsonify }}, {% for f in fields %}{{ f.field | jsonify }}: {% if item[f.field] %}{{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }}{% else %}"none"{% endif %}{% unless forloop.last %},{% endunless %}{% endfor %} }{%- unless forloop.last -%},{%- endunless -%}
